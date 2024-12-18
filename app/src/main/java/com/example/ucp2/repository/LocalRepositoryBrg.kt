4545a44259c913ad8b13e214945f79ba2c124592
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-18 14:40:53 +0700

    menambahkan Local repository barang yang menghubungkan BarangDao dengan operasi2 di RepositoryBrg

---
+++ app/src/main/java/com/example/ucp2/repository/LocalRepositoryBrg.kt
@@ -0,0 +1,30 @@
+package com.example.ucp2.repository
+
+import com.example.ucp2.data.dao.BarangDao
+import com.example.ucp2.data.entity.Barang
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryBrg(
+    private val barangDao: BarangDao
+): RepositoryBrg {
+    override suspend fun insertBrg(barang: Barang) {
+        barangDao.insertBarang(barang)
+    }
+
+    override fun getAllBrg(): Flow<List<Barang>> {
+        return barangDao.getAllBarang()
+    }
+
+    override fun getBrg(id: String): Flow<Barang> {
+        return barangDao.getBarang(id)
+    }
+
+    override suspend fun deleteBrg(barang: Barang) {
+        return barangDao.deleteBarang(barang)
+    }
+
+    override suspend fun updateBrg(barang: Barang) {
+        return barangDao.updateBarang(barang)
+    }
+
+}
