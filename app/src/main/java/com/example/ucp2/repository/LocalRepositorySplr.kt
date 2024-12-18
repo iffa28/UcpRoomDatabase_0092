fbf42c6d9b08fbf70fab0683a3b838e5ffc6bc02
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-18 14:45:19 +0700

    menambahkan Local repository suplier yang menghubungkan suplierDao dengan operasi2 di RepositorySplr

---
+++ app/src/main/java/com/example/ucp2/repository/LocalRepositorySplr.kt
@@ -0,0 +1,30 @@
+package com.example.ucp2.repository
+
+import com.example.ucp2.data.dao.SuplierDao
+import com.example.ucp2.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositorySplr(
+    private val suplierDao: SuplierDao
+): RepositorySplr {
+    override suspend fun insertSplr(suplier: Suplier) {
+        suplierDao.insertSuplier(suplier)
+    }
+
+    override fun getAllSplr(): Flow<List<Suplier>> {
+        return suplierDao.getAllSuplier()
+    }
+
+    override fun getSplr(id: String): Flow<Suplier> {
+        return suplierDao.getSuplier(id)
+    }
+
+    override suspend fun deleteSplr(suplier: Suplier) {
+        return suplierDao.deleteSuplier(suplier)
+    }
+
+    override suspend fun updateSplr(suplier: Suplier) {
+        return suplierDao.updateSuplier(suplier)
+    }
+
+}
