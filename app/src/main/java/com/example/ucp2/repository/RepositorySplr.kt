b70e0869b53bb7c3bf14c30d98c37cbbe4b75f64
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-18 14:30:43 +0700

    menambahkan metode metode untuk mengelola data suplier

---
+++ app/src/main/java/com/example/ucp2/repository/RepositorySplr.kt
@@ -0,0 +1,16 @@
+package com.example.ucp2.repository
+
+import com.example.ucp2.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
+
+interface RepositorySplr {
+    suspend fun insertSplr(suplier: Suplier)
+
+    fun getAllSplr(): Flow<List<Suplier>>
+
+    fun getSplr(id: String): Flow<Suplier>
+
+    suspend fun deleteSplr(suplier: Suplier)
+
+    suspend fun updateSplr(suplier: Suplier)
+}
