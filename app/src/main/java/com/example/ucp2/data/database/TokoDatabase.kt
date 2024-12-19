80e6fa60fb96af4de9e34011dc00484a75e8f499
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-19 11:47:54 +0700

    membuat database untuk menyimpan entitas entitas

---
+++ app/src/main/java/com/example/ucp2/data/database/TokoDatabase.kt
@@ -0,0 +1,35 @@
+package com.example.ucp2.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.ucp2.data.dao.BarangDao
+import com.example.ucp2.data.dao.SuplierDao
+import com.example.ucp2.data.entity.Barang
+import com.example.ucp2.data.entity.Suplier
+
+@Database(entities = [Barang::class, Suplier::class], version= 1, exportSchema = false )
+abstract class TokoDatabase: RoomDatabase() {
+
+    abstract fun barangDao(): BarangDao
+    abstract fun suplierDao(): SuplierDao
+
+    companion object{
+        @Volatile
+        private var Instance: TokoDatabase? = null
+
+        fun getDatabase(context: Context): TokoDatabase {
+            return (Instance ?: synchronized(this)
+            {
+                Room.databaseBuilder(
+                    context,
+                    TokoDatabase::class.java,         //Class database
+                    "TokoDatabase"              //Nama database
+                )
+                    .build().also { Instance = it }
+            })
+        }
+
+    }
+}
