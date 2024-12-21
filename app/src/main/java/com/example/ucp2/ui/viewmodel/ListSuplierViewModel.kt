d8fcc935ecc46901759844c9eb0647ec13d544d8
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-21 18:10:26 +0700

    menambahkan view model suplier untuk menampilkan list suplier dari repositorySplr

--- app/src/main/java/com/example/ucp2/ui/viewmodel/ListSuplierViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/ListSuplierViewModel.kt
@@ -1,6 +1,49 @@
 package com.example.ucp2.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.data.entity.Suplier
+import com.example.ucp2.repository.RepositorySplr
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class ListSplrViewModel(
+    private val repositorySplr: RepositorySplr
+): ViewModel() {
+    val listSuplierUiState: StateFlow<ListSuplierUiState> = repositorySplr.getAllSplr()
+        .filterNotNull()
+        .map {
+            ListSuplierUiState(
+                listSplr = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(ListSuplierUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                ListSuplierUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = ListSuplierUiState(
+                isLoading = true,)
+        )
+}
 
 data class ListSuplierUiState(
     val listSplr: List<Suplier> = listOf(),
