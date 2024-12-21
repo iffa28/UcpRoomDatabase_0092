1b5435c53e269ffd370cd341415e13325fbed2e6
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-21 16:11:12 +0700

    membuat dataclass untuk mengecek validasi error

--- app/src/main/java/com/example/ucp2/ui/viewmodel/InsertSuplierViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/InsertSuplierViewModel.kt
@@ -2,6 +2,16 @@ package com.example.ucp2.ui.viewmodel
 
 import com.example.ucp2.data.entity.Suplier
 
+data class FormSplrErrorState(
+    val namaSuplier: String? = null,
+    val kontak: String? = null,
+    val alamat: String? = null,
+){
+    fun isValid(): Boolean {
+        return namaSuplier == null && kontak == null && alamat == null
+    }
+}
+
 fun SuplierEvent.toSuplierEntity(): Suplier = Suplier(
     namaSuplier = namaSuplier,
     kontak = kontak,
