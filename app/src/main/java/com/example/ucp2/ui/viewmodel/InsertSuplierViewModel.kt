895337fa827057edc1db5a406a6116c236641544
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-21 16:08:34 +0700

    membuat fungsi untuk mengubah SuplierEvent menjadi entitas Suplier

--- app/src/main/java/com/example/ucp2/ui/viewmodel/InsertSuplierViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/InsertSuplierViewModel.kt
@@ -1,5 +1,13 @@
 package com.example.ucp2.ui.viewmodel
 
+import com.example.ucp2.data.entity.Suplier
+
+fun SuplierEvent.toSuplierEntity(): Suplier = Suplier(
+    namaSuplier = namaSuplier,
+    kontak = kontak,
+    alamat = alamat
+)
+
 data class SuplierEvent(
     val namaSuplier: String = "",
     val kontak: String = "",
