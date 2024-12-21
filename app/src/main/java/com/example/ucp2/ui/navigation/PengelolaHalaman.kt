2eda5d7245478962c33aee98f50d0e7deb77d7dd
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-21 14:29:51 +0700

    membuat navigasi halaman home

---
+++ app/src/main/java/com/example/ucp2/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,34 @@
+package com.example.ucp2.ui.navigation
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+import androidx.navigation.NavHostController
+
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.ucp2.ui.view.HomeView
+import com.example.ucp2.ui.view.InsertBrgView
+import com.example.ucp2.ui.view.InsertSuplierView
+
+
+@Composable
+fun PengelolaHalaman(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route) {
+        composable(
+            route = DestinasiHome.route
+        ) {
+            HomeView(
+                modifier = modifier
+            )
+        }
+
+    }
+
+
+}
