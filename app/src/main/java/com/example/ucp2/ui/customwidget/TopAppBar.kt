2d4f434c5adb1ffc201f2e0e9b175c213d68862f
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-24 12:34:20 +0700

    menambahkan background, size, gambar icon, dan warna teks

--- app/src/main/java/com/example/ucp2/ui/customwidget/TopAppBar.kt
+++ app/src/main/java/com/example/ucp2/ui/customwidget/TopAppBar.kt
@@ -1,18 +1,31 @@
 package com.example.ucp2.ui.customwidget
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.ucp2.R
 
 @Composable
 fun TopAppBar(
@@ -23,7 +36,9 @@ fun TopAppBar(
 ) {
     Box(modifier = Modifier
         .fillMaxWidth()
-        .padding(16.dp),
+        .background(color = colorResource(id = R.color.Ctophome)
+        )
+        .size(100.dp),
         contentAlignment = Alignment.Center
     ){
         if (showBackButton) {
@@ -35,7 +50,14 @@ fun TopAppBar(
                     onClick = onBack,
                     modifier = Modifier.align(Alignment.CenterVertically)
                 ) {
-                    Text("Kembali")
+                    Column {
+                        Image(
+                            painter = painterResource(id = R.drawable.undo),
+                            contentDescription = "", Modifier
+                                .size(45.dp)
+                                .padding(5.dp)
+                        )
+                    }
                 }
                 Spacer(modifier = Modifier.weight(2f))
 
@@ -44,7 +66,8 @@ fun TopAppBar(
 
         Text(
             text = judul,
-            fontSize = 25.sp,
+            fontSize = 30.sp,
+            color = Color.White,
             fontWeight = FontWeight.Bold,
             modifier = Modifier.align(Alignment.Center)
         )
