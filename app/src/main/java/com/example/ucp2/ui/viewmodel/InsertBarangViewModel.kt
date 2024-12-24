e863ba6b8da4bc33d5187eacb4414064731d34b1
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-24 14:32:13 +0700

    menambahkan fungsi untuk menyimpan input ke entity

--- app/src/main/java/com/example/ucp2/ui/viewmodel/InsertBarangViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/InsertBarangViewModel.kt
@@ -1,5 +1,24 @@
 package com.example.ucp2.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Barang
+import com.example.ucp2.repository.RepositoryBrg
+import kotlinx.coroutines.launch
+
+
+//Menyimpan input form ke dalam entity
+fun BarangEvent.toBarangEntity(): Barang = Barang(
+    id= id,
+    nama = nama,
+    deskripsi = deskripsi,
+    harga = harga,
+    stok = stok.toIntOrNull() ?: 0,       // Konversi String ke Int
+    namaSuplier = namaSuplier
+)
 
 
 data class FormErrorState(
