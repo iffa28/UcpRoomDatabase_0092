b328381e22bd66a4cd98e6bd40c3e54aae038c2e
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-24 12:48:08 +0700

    menambahkan fungsi untuk body untuk list Suplier

--- app/src/main/java/com/example/ucp2/ui/view/ListSuplierView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/ListSuplierView.kt
@@ -2,9 +2,11 @@ package com.example.ucp2.ui.view
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,10 +16,15 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -26,6 +33,59 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucp2.R
 import com.example.ucp2.data.entity.Suplier
+import com.example.ucp2.ui.viewmodel.ListSuplierUiState
+import kotlinx.coroutines.launch
+
+
+@Composable
+fun BodyListSplrView(
+    listSuplierUiState: ListSuplierUiState,
+    modifier: Modifier = Modifier
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    when {
+        listSuplierUiState.isLoading -> {
+            //menampilkan indikator loading
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+        listSuplierUiState.isError -> {
+            LaunchedEffect(listSuplierUiState.errorMessage) {
+                listSuplierUiState.errorMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message)
+                    }
+                }
+            }
+        }
+
+        listSuplierUiState.listSplr.isEmpty() -> {
+            //menampilkan pesan jika data kosong
+            Box(modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Tidak ada data Suplier.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+        else -> {
+            //Menampilkan daftar mahasiswa
+            ListSuplier(
+                listSplr = listSuplierUiState.listSplr,
+                modifier = modifier
+            )
+        }
+
+    }
+}
 
 
 @Composable
